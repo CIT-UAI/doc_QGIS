c5364b79c1077648105aa7e177e913e0e6a5e35b
latot <felipematas@yahoo.com>
2022-04-22 11:01:52 -0400

    upd doc

--- _build.sh
+++ _build.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
+rm -rf docs
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
